44c6b0acdae2d660aa2a69e306a50b5ec72fe519
Ajeet <ajeetsharma369@gmail.com>
2022-06-18 16:48:12 +0530

    First time by ajeet

---
+++ app/src/main/java/com/microware/planindia/views/homescreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.microware.planindia.views.homescreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microware.planindia.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
